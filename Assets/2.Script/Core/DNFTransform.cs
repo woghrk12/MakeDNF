9b1e77421b80b93455c87a28685372342b1436f8
LAPTOP-7K9L41UJ\hjh22 <hjh222427@gmail.com>
2023-11-29 20:34:10 +0900

    Modify the class name (CharacterTransform -> DNFTransform)

--- Assets/2.Script/Core/DNFTransform.cs
+++ Assets/2.Script/Core/DNFTransform.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterTransform : MonoBehaviour
+public class DNFTransform : MonoBehaviour
 {
     #region Variables
 
