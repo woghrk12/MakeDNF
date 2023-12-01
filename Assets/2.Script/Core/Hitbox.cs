f413c1d549c7f6a232721815ec06817d47803df3
LAPTOP-7K9L41UJ\hjh22 <hjh222427@gmail.com>
2023-12-01 18:40:03 +0900

    Modify the hitbox variable from float to vector3 type

--- Assets/2.Script/Core/Hitbox.cs
+++ Assets/2.Script/Core/Hitbox.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public enum EHitboxType { NONE = -1, BOX, CIRCLE, END }
+public enum EHitboxType { NONE = -1, BOX, CIRCLE }
 
 [RequireComponent(typeof(DNFTransform))]
 public class Hitbox : MonoBehaviour
@@ -12,23 +12,22 @@ public class Hitbox : MonoBehaviour
     [SerializeField] private EHitboxType hitboxType = EHitboxType.NONE;
 
     [Header("Variables for the hitbox shape")]
-    [HideInInspector] public float SizeX = 0f;
-    [HideInInspector] public float SizeY = 0f;
-    [HideInInspector] public float SizeZ = 0f;
-    [HideInInspector] public float OffsetX = 0f;
-    [HideInInspector] public float OffsetY = 0f;
-    [HideInInspector] public float OffsetZ = 0f;
-    [HideInInspector, Range(0f, 1f)] public float PivotX = 0f;
-    [HideInInspector, Range(0f, 1f)] public float PivotY = 0f;
-    [HideInInspector, Range(0f, 1f)] public float PivotZ = 0f;
-    
+    [HideInInspector] public Vector3 Size = new Vector3(1f, 1f, 1f);
+    [HideInInspector] public Vector3 Offset = Vector3.zero;
+    [HideInInspector] public Vector3 Pivot = Vector3.zero;
+
     private Vector3 maxHitboxPos = Vector3.zero;
     private Vector3 minHitboxPos = Vector3.zero;
 
     #endregion Variable
 
+    #region Properties
+
+    public EHitboxType HitboxType => hitboxType;
     public Vector3 MaxHitboxPos => maxHitboxPos;
     public Vector3 MinHitboxPos => minHitboxPos;
+
+    #endregion Properties
 
     #region Unity Events
 
@@ -37,11 +36,9 @@ public class Hitbox : MonoBehaviour
         dnfTransform = GetComponent<DNFTransform>();
     }
 
-    private void OnValidate()
+    private void Update()
     {
-        if (SizeX < 0f) SizeX = 0f;
-        if (SizeY < 0f) SizeY = 0f;
-        if (SizeZ < 0f) SizeZ = 0f;
+        CalculateHitbox();
     }
 
     #endregion Unity Events
@@ -51,11 +48,12 @@ public class Hitbox : MonoBehaviour
     public void CalculateHitbox()
     {
         Vector3 position = dnfTransform.Position;
+        float localScale = dnfTransform.LocalScale;
 
-        position += new Vector3(OffsetX, OffsetY, OffsetZ);
+        position += Offset;
 
-        minHitboxPos = position - new Vector3(SizeX * PivotX, SizeY * PivotY, SizeZ * PivotZ);
-        maxHitboxPos = position + new Vector3(SizeX * (1f - PivotX), SizeY * (1f - PivotY), SizeZ * (1f - PivotZ));
+        minHitboxPos = position - new Vector3(Size.x * Pivot.x * localScale, Size.y * Pivot.y * localScale, Size.z * Pivot.z * localScale);
+        maxHitboxPos = position + new Vector3(Size.x * (1f - Pivot.x) * localScale, Size.y * (1f - Pivot.y) * localScale, Size.z * (1f - Pivot.z) * localScale);
     }
 
     #endregion Methods
