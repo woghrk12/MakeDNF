913d26bab1406512fcc2f43c6065e8ef9a665c2d
LAPTOP-7K9L41UJ\hjh22 <hjh222427@gmail.com>
2023-12-06 19:24:31 +0900

    Add a class for the Singleton design pattern

---
+++ Assets/2.Script/Util/SingletonBehaviour.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingletonBehaviour<T> : MonoBehaviour where T : Component
+{
+    #region Variables
+
+    private static T instance = null;
+
+    #endregion Variables
+
+    #region Properties
+
+    public static T Instance
+    {
+        get
+        {
+            if (ReferenceEquals(instance, null))
+            {
+                instance = FindObjectOfType(typeof(T)) as T;
+                
+                if (ReferenceEquals(instance, null))
+                {
+                    instance = new GameObject(typeof(T).Name, typeof(T)).GetComponent<T>();
+                }
+            }
+
+            return instance;
+        }
+    }
+
+    #endregion Properties
+
+    #region Unity Events
+
+    protected virtual void Awake()
+    {
+        T[] objects = FindObjectsOfType<T>();
+
+        if (objects.Length > 1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        if (transform.parent != null && transform.root != null)
+        {
+            DontDestroyOnLoad(transform.root.gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    #endregion Unity Events
+}
