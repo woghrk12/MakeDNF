12f360d67cf670e127ef1376609cf67aaedf4e98
LAPTOP-7K9L41UJ\hjh22 <hjh222427@gmail.com>
2023-12-06 19:24:03 +0900

    Add input manager to control player input

---
+++ Assets/2.Script/Manager/InputManager.cs
@@ -0,0 +1,53 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager
+{
+    #region Variables
+
+    public PlayerJoystick playerJoystick = null;
+    public Dictionary<EKeyName, PlayerButton> playerButtonDictionary = new();
+
+    #endregion Variables
+
+    #region Properties
+
+    public PlayerJoystick PlayerJoystick => playerJoystick;
+    public Dictionary<EKeyName, PlayerButton> PlayerButtonDictionary => playerButtonDictionary;
+
+    #endregion Properties
+
+    public void Init()
+    {
+        Transform inputSystem = GameManager.Resource.Instantiate(
+#if UNITY_EDITOR || UNITY_STANDALONE
+            ResourcePath.KEYBOARD_INPUT_SYSTEM
+#else
+            ResourcePath.SCREEN_INPUT_SYSTEM
+#endif
+            , GameManager.Instance.transform).transform;
+
+        playerJoystick = inputSystem.GetChild(0).GetComponent<PlayerJoystick>();
+        
+        int childCount = inputSystem.childCount;
+        for (int i = 1; i < childCount; i++)
+        {
+            if (!inputSystem.GetChild(i).TryGetComponent(out PlayerButton button))
+            {
+                throw new System.Exception($"There is no button component. Name : {inputSystem.GetChild(i).name}");
+            }
+
+            if (button.KeyName == EKeyName.NONE)
+            {
+                throw new System.Exception($"Button is not setting. Name : {button.name}");
+            }
+
+            if (playerButtonDictionary.ContainsKey(button.KeyName))
+            {
+                throw new System.Exception($"Button is already exist. Previous : {playerButtonDictionary[button.KeyName].name} / New : {button.name}");
+            }
+
+            playerButtonDictionary.Add(button.KeyName, button);   
+        }
+    }
+}
