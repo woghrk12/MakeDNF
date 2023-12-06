016183fef1b78c087e69de485af5ba63394b8178
LAPTOP-7K9L41UJ\hjh22 <hjh222427@gmail.com>
2023-12-06 21:35:06 +0900

    Modify to allow setting directional keys on the keyboard joystick

--- Assets/2.Script/Core/Input/PC/KeyboardJoystick.cs
+++ Assets/2.Script/Core/Input/PC/KeyboardJoystick.cs
@@ -1,15 +1,60 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class KeyboardJoystick : PlayerJoystick
 {
+    [SerializeField] private KeyCode upKeyCode = KeyCode.None;
+    [SerializeField] private KeyCode downKeyCode = KeyCode.None;
+    [SerializeField] private KeyCode leftKeyCode = KeyCode.None;
+    [SerializeField] private KeyCode rightKeyCode = KeyCode.None;
+
+    private int lastPressedHorizontal = 0;
+    private int lastPressedVertical = 0;
+    
     protected override void SetDirection()
     {
-        float h = Input.GetAxisRaw("Horizontal");
-        float v = Input.GetAxisRaw("Vertical");
+        bool isLeft = Input.GetKey(leftKeyCode);
+        bool isRight = Input.GetKey(rightKeyCode);
+        bool isUp = Input.GetKey(upKeyCode);
+        bool isDown = Input.GetKey(downKeyCode);
 
-        moveDirection.x = h;
-        moveDirection.z = v;
+        if (isLeft && isRight)
+        {
+            moveDirection.x = lastPressedHorizontal * -1;
+        }
+        else if (isLeft)
+        {
+            moveDirection.x = -1;
+            lastPressedHorizontal = -1;
+        }
+        else if (isRight)
+        {
+            moveDirection.x = 1;
+            lastPressedHorizontal = 1;
+        }
+        else
+        {
+            moveDirection.x = 0;
+            lastPressedHorizontal = 0;
+        }
+
+        if (isUp && isDown)
+        {
+            moveDirection.z = lastPressedVertical * -1;
+        }
+        else if (isDown)
+        {
+            moveDirection.z = -1;
+            lastPressedVertical = -1;
+        }
+        else if (isUp)
+        {
+            moveDirection.z = 1;
+            lastPressedVertical = 1;
+        }
+        else
+        {
+            moveDirection.z = 0;
+            lastPressedVertical = 0;
+        }
     }
 }
