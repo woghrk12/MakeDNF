ee1b51bb45fd2498c6a9d2d1a16c25442a88c82a
LAPTOP-7K9L41UJ\hjh22 <hjh222427@gmail.com>
2023-12-06 19:08:34 +0900

    Add a class to contain file paths

--- Assets/2.Script/GlobalDefine.cs
+++ Assets/2.Script/GlobalDefine.cs
@@ -5,4 +5,13 @@ public class GlobalDefine
     public static readonly float ConvRate = (float)yRate / xRate;
     public static readonly float InvConvRate = (float)xRate / yRate;
 }
+
+public class ResourcePath
+{
+    public static readonly string PREFAB = "Prefab";
+    public static readonly string PREFAB_INPUT = "Prefab/Input";
+    public static readonly string KEYBOARD_INPUT_SYSTEM = "Prefab/Input/Keyboard Input System";
+    public static readonly string SCREEN_INPUT_SYSTEM = "Prefab/Input/Screen Input System";
+}
+
 public enum EKeyName { NONE = -1, BASEATTACK, JUMP, SKILL1, SKILL2, SKILL3, SKILL4, INVENTORY, SKILL, STATUS }
