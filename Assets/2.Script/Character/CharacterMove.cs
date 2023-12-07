557b1d8b597f8b3ebbf0cce015dc54567d9cdcca
LAPTOP-7K9L41UJ\hjh22 <hjh222427@gmail.com>
2023-12-07 16:03:12 +0900

    Connect character movement function to input manager

--- Assets/2.Script/Character/CharacterMove.cs
+++ Assets/2.Script/Character/CharacterMove.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 public class CharacterMove : MonoBehaviour
 {
     private DNFTransform dnfTransform = null;
-    private PlayerJoystick joystick = null;
 
     [SerializeField] private float xMoveSpeed = 1f;
     [SerializeField] private float zMoveSpeed = 1f;
@@ -16,12 +15,7 @@ public class CharacterMove : MonoBehaviour
 
     private void Start()
     {
-        joystick = GameManager.Input.PlayerJoystick;
-    }
-
-    private void FixedUpdate()
-    {
-        Move(joystick.MoveDirection);
+        GameManager.Input.SetMovementDelegate(Move);
     }
 
     public void Move(Vector3 moveDir)
