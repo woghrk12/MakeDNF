ec7a9bbdb89bda729f80ad7976e347e0a911c032
LAPTOP-7K9L41UJ\hjh22 <hjh222427@gmail.com>
2023-12-07 22:36:19 +0900

    Add a class to store animation keys

---
+++ Assets/2.Script/Util/AnimatorKey.cs
@@ -0,0 +1,10 @@
+public class AnimatorKey
+{
+    public class Character
+    {
+        public static readonly string MOTION_SPEED = "MotionSpeed";
+        public static readonly string IS_WALK = "IsWalk";
+        public static readonly string DO_JUMP = "DoJump";
+        public static readonly string IS_JUMP = "IsJump";
+    }
+}
