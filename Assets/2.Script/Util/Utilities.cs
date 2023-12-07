e9ac3b832907101e04d85d37fea8434101d0b374
LAPTOP-7K9L41UJ\hjh22 <hjh222427@gmail.com>
2023-12-07 22:34:02 +0900

    Add Utilities class

---
+++ Assets/2.Script/Util/Utilities.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Utilities
+{
+    #region Variables
+
+    /// <summary>
+    /// Cached WaitForFixedUpdate object.
+    /// Wait until next fixed frame rate update function.
+    /// </summary>
+    public static readonly WaitForFixedUpdate WaitForFixedUpdate = new();
+
+    /// <summary>
+    /// Dictionary for cached WaitForSeconds objects.
+    /// <para> Key : Amount of seconds. </para>
+    /// <para> Value : Suspend the coroutine execution for the amount of seconds, which is given as Key, using scaled time. </para>
+    /// </summary>
+    private static readonly Dictionary<float, WaitForSeconds> waitForSecondsDictionary = new();
+
+    #endregion Variables
+
+    #region Methods
+
+    /// <summary>
+    /// Return the WaitForSeconds object corresponding the time passed as a parameter.
+    /// Return a cached object or creates new object if there is no cached object.
+    /// </summary>
+    /// <returns>The cached or new WaitForSeconds object corresponding the time</returns>
+    public static WaitForSeconds WaitForSeconds(float second)
+    {
+        if (waitForSecondsDictionary.ContainsKey(second))
+        {
+            return waitForSecondsDictionary[second];
+        }
+
+        WaitForSeconds waitForSeconds = new(second);
+        waitForSecondsDictionary.Add(second, waitForSeconds);
+        return waitForSeconds;
+    }
+
+    #endregion Methods
+}
