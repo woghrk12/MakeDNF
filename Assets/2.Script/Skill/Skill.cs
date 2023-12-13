7e5e8b16f8c65a3936abf08d0eb9b1f1d9388bb8
LAPTOP-7K9L41UJ\hjh22 <hjh222427@gmail.com>
2023-12-13 21:23:30 +0900

    Add skill class

---
+++ Assets/2.Script/Skill/Skill.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+[Serializable]
+public struct SkillStat
+{
+    public string Name;
+    public int NeedMana;
+    public string SkillDescription;
+}
+
+public abstract class Skill : MonoBehaviour
+{
+    #region Variables
+
+    [SerializeField] protected SkillStat skillStat;
+
+    #endregion Variables
+
+    #region Properties
+
+    public string Name => skillStat.Name;
+    public int NeedMana => skillStat.NeedMana;
+    public string SkillDescription => skillStat.SkillDescription;
+
+    #endregion Properties
+
+    #region Methods
+
+    public virtual void OnPressed() { }
+    public virtual void OnReleased() { }
+
+    public abstract IEnumerator ActivateSkill();
+
+    public abstract void Clear();
+
+    #endregion Methods
+}
