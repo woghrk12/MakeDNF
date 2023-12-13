b28a44881dbdfffba8cd4845507a39d703767015
LAPTOP-7K9L41UJ\hjh22 <hjh222427@gmail.com>
2023-12-13 21:25:40 +0900

    Modify the logic to use the skill and add the functionality to register the skill

--- Assets/2.Script/Character/Function/CharacterAttack.cs
+++ Assets/2.Script/Character/Function/CharacterAttack.cs
@@ -6,19 +6,58 @@ public class CharacterAttack : MonoBehaviour
 {
     #region Variables
 
+    private Dictionary<EKeyName, Skill> registeredSkillDictionary = new();
+
     #endregion Variables
 
     #region Methods
 
-    public void UseSkill(EKeyName keyName)
+    public void RegisterSkill(EKeyName keyName, Skill skill)
     {
-        Debug.Log(keyName + " Skill use.");
+        switch (keyName)
+        {
+            case EKeyName.BASEATTACK:
+            case EKeyName.SKILL1:
+            case EKeyName.SKILL2:
+            case EKeyName.SKILL3:
+            case EKeyName.SKILL4:
+                if (registeredSkillDictionary.ContainsKey(keyName))
+                {
+                    registeredSkillDictionary[keyName] = skill;
+                }
+                else
+                {
+                    registeredSkillDictionary.Add(keyName, skill);
+                }
+                break;
+
+            default:
+                throw new System.Exception($"Unable key name. Input key name : {keyName}.");
+        }
     }
 
-    public void ReleaseSkill(EKeyName keyName)
+    public IEnumerator UseSkill(EKeyName keyName)
     {
-        Debug.Log(keyName + " Skill release.");
+        yield return registeredSkillDictionary[keyName].ActivateSkill();
     }
+
+    #region Events
+
+    public void OnSkillButtonPressed(EKeyName keyName)
+    {
+        if (!registeredSkillDictionary.ContainsKey(keyName)) return;
+        
+        registeredSkillDictionary[keyName].OnPressed();
+    }
+
+    public void OnSkillButtonReleased(EKeyName keyName)
+    {
+        if (!registeredSkillDictionary.ContainsKey(keyName)) return;
+
+        registeredSkillDictionary[keyName].OnReleased();
+    }
+
+    #endregion Events
 
     #endregion Methods
 }
