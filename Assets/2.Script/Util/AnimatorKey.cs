ea1d39de1427f4321ee7bcdfcb411aa37a82783a
LAPTOP-7K9L41UJ\hjh22 <hjh222427@gmail.com>
2023-12-15 19:52:53 +0900

    Divide the motion speed parameter of animator to move speed and attack speed

--- Assets/2.Script/Util/AnimatorKey.cs
+++ Assets/2.Script/Util/AnimatorKey.cs
@@ -2,7 +2,9 @@ public class AnimatorKey
 {
     public class Character
     {
-        public static readonly string MOTION_SPEED = "MotionSpeed";
+        public static readonly string MOVE_SPEED = "MoveSpeed";
+        public static readonly string ATTACK_SPEED = "AttackSpeed";
+
         public static readonly string IS_WALK = "IsWalk";
         public static readonly string DO_JUMP = "DoJump";
         public static readonly string IS_JUMP = "IsJump";
