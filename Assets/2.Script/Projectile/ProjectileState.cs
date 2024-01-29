bf55f38d37fa942428766940a1f9303ec0ae6395
LAPTOP-7K9L41UJ\hjh22 <hjh222427@gmail.com>
2024-01-29 22:02:27 +0900

    Add enum for checking the timing of the projectile state

--- Assets/2.Script/Projectile/ProjectileState.cs
+++ Assets/2.Script/Projectile/ProjectileState.cs
@@ -4,6 +4,18 @@ using UnityEngine;
 
 public abstract class ProjectileState
 {
+    /// <summary>
+    /// The phase of the skill state.
+    /// <para>
+    /// PREDELAY : Delay before hitbox activation or projectile creation.
+    /// HITBOXACTIVE : Phase during hitbox activation.
+    /// STOPMOTION : Phase for stiffness effect if any object has been hit by the skill.
+    /// MOTIONINPROGRESS : Phase after hitbox deactivation or projectile creation.
+    /// POSTDELAY : Delay after the skill motion animation ends.
+    /// </para>
+    /// </summary>
+    protected enum EStatePhase { NONE = -1, PREDELAY, HITBOXACTIVE, STOPMOTION, MOTIONINPROGRESS, POSTDELAY }
+
     #region Constructor
 
     public ProjectileState(Projectile stateController) { }
