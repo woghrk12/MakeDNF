e29d2ac87c1fb20171622274fb63c5b61cc95de7
LAPTOP-7K9L41UJ\hjh22 <hjh222427@gmail.com>
2024-01-31 01:06:22 +0900

    Add the update logic for following visual effect

--- Assets/2.Script/Effect/FollowingVFX.cs
+++ Assets/2.Script/Effect/FollowingVFX.cs
@@ -1,4 +1,11 @@
+using UnityEngine;
+
 public class FollowingVFX : VFX
 {
-   
+    private void FixedUpdate()
+    {
+        if (targetTransform == null) return;
+
+        cachedTransform.position = new Vector3(targetTransform.Position.x, targetTransform.Position.y + targetTransform.Position.z * GlobalDefine.CONV_RATE, 0f);
+    }
 }
