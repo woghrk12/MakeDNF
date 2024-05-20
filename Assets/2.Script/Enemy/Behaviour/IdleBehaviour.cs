91f1e8660692225d8966189a8c14551450d8f6a1
LAPTOP-7K9L41UJ\hjh22 <hjh222427@gmail.com>
2024-05-20 22:55:19 +0900

    Add idle behaviour for the enemy

---
+++ Assets/2.Script/Enemy/Behaviour/IdleBehaviour.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace EnemyFSM
+{
+    /// <summary>
+    /// The behaviour class when the enemy is idle state.
+    /// </summary>
+    [CreateAssetMenu(fileName = "New IdleBehaviour", menuName = "Enemy AI/Idle Behaviour")]
+    public class IdleBehaviour : EnemyBehaviour
+    {
+        
+    }
+}
