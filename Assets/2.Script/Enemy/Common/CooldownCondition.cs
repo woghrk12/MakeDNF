ed86b23b68f1be81c09f2a4ef07c967ffcc74152
LAPTOP-7K9L41UJ\hjh22 <hjh222427@gmail.com>
2024-06-03 23:07:24 +0900

    Add the condition node that checking the cooldown

---
+++ Assets/2.Script/Enemy/Common/CooldownCondition.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+namespace BehaviourTree
+{
+    public class CooldownCondition : Condition
+    {
+        #region Variables
+
+        private string variableName = string.Empty;
+
+        private float cooldown = 0f;
+
+        #endregion Variables
+
+        #region Constructor
+
+        public CooldownCondition(string variableName, float cooldown, Enemy controller) : base(controller)
+        {
+            this.variableName = variableName;
+            this.cooldown = cooldown;
+        }
+
+        #endregion Constructor
+
+        protected override bool CheckCondition(Blackboard blackboard)
+        {
+            if (!blackboard.TryGetVariable(variableName, out FloatVariable variable))
+            {
+                variable = new FloatVariable();
+                blackboard.AddVariable(variableName, variable);
+
+                return false;
+            }
+
+            if (variable.Value < cooldown)
+            {
+                variable.Value += Time.deltaTime;
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
