d8d062f00d2c12e109db45c73ce9d2350533cf01
LAPTOP-7K9L41UJ\hjh22 <hjh222427@gmail.com>
2024-06-06 16:31:07 +0900

    Add editor class to set the data of the behaviour tree by using UI Builder

---
+++ Assets/2.Script/Enemy/BehaviourTree/Editor/BehaviourTreeEditor.cs
@@ -0,0 +1,28 @@
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class BehaviourTreeEditor : EditorWindow
+{
+    [MenuItem("Tools/Data/Behaviour Tree Tool")]
+    public static void Init()
+    {
+        BehaviourTreeEditor wnd = GetWindow<BehaviourTreeEditor>();
+        wnd.titleContent = new GUIContent("BehaviourTreeEditor");
+    }
+
+    public void CreateGUI()
+    {
+        // Each editor window contains a root VisualElement object
+        VisualElement root = rootVisualElement;
+
+        // Import UXML
+        var visualTree = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/2.Script/Enemy/BehaviourTree/Editor/BehaviourTreeEditor.uxml");
+        visualTree.CloneTree(root);
+
+        // A stylesheet can be added to a VisualElement
+        // The style will be applied to the VisualElement and all of its children
+        var styleSheet = AssetDatabase.LoadAssetAtPath<StyleSheet>("Assets/2.Script/Enemy/BehaviourTree/Editor/BehaviourTreeEditor.uss");
+        root.styleSheets.Add(styleSheet);
+    }
+}
