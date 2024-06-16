2b3dba70efa8b6eec1cba559e009bc38dae37b66
LAPTOP-7K9L41UJ\hjh22 <hjh222427@gmail.com>
2024-06-16 17:43:06 +0900

    Remove unnecessary methods from the Behavior Tree class and modify it to inherit from MonoBehaviour

--- Assets/2.Script/Enemy/BehaviourTree/BehaviourTree.cs
+++ Assets/2.Script/Enemy/BehaviourTree/BehaviourTree.cs
@@ -1,163 +1,39 @@
-using System;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEditor;
 
 namespace BehaviourTree
 {
-    public class BehaviourTree : ScriptableObject
     [DisallowMultipleComponent]
+    public class BehaviourTree : MonoBehaviour
     {
         #region Variables
 
-        public Node RootNode = null;
-
-        public List<Node> NodeList = new();
+        private RootNode rootNode = null;
 
         #endregion Variables
+
+        #region Unity Events
+
+        private void Awake()
+        {
+            if (!TryGetComponent(out RootNode rootNode))
+            {
+                Debug.LogWarning("Missing root node in behaviour tree.", this);
+                return;
+            }
+
+            this.rootNode = rootNode;
+        }
+
+        #endregion Unity Events
 
         #region Methods
 
         public void Run()
         {
-            if (ReferenceEquals(RootNode, null)) return;
-            if (RootNode.State != ENodeState.RUNNING) return;
+            if (ReferenceEquals(rootNode, null)) return;
+            if (rootNode.State != ENodeState.RUNNING) return;
 
-            RootNode.Evaluate();
-        }
-
-        public Node AddNode(Type type)
-        {
-            Node node = ScriptableObject.CreateInstance(type) as Node;
-
-            node.name = type.Name;
-            node.GUID = GUID.Generate().ToString();
-
-            Undo.RecordObject(this, "Behaviour Tree (CreateNode)");
-
-            NodeList.Add(node);
-
-            if (!Application.isPlaying)
-            {
-                AssetDatabase.AddObjectToAsset(node, this);
-            }
-
-            Undo.RegisterCreatedObjectUndo(node, "Behaviour Tree (AddNode)");
-
-            AssetDatabase.SaveAssets();
-
-            return node;
-        }
-
-        public void RemoveNode(Node node)
-        {
-            Undo.RecordObject(this, "Behaviour Tree (RemoveNode)");
-
-            NodeList.Remove(node);
-
-            //AssetDatabase.RemoveObjectFromAsset(node);
-            Undo.DestroyObjectImmediate(node);
-
-            AssetDatabase.SaveAssets();
-        }
-
-        public void AddChildNode(Node parentNode, Node childNode)
-        {
-            if (parentNode is RootNode)
-            {
-                Undo.RecordObject(parentNode, "Behaviour Tree (AddChildNode)");
-
-                (parentNode as RootNode).ChildNode = childNode;
-
-                EditorUtility.SetDirty(parentNode);
-            }
-            else if (parentNode is DecoratorNode)
-            {
-                Undo.RecordObject(parentNode, "Behaviour Tree (AddChildNode)");
-
-                (parentNode as DecoratorNode).ChildNode = childNode;
-
-                EditorUtility.SetDirty(parentNode);
-            }
-            else if (parentNode is CompositeNode)
-            {
-                Undo.RecordObject(parentNode, "Behaviour Tree (AddChildNode)");
-
-                (parentNode as CompositeNode).ChildNodeList.Add(childNode);
-
-                EditorUtility.SetDirty(parentNode);
-            }
-        }
-
-        public void RemoveChildNode(Node parentNode, Node childNode)
-        {
-            if (parentNode is RootNode)
-            {
-                Undo.RecordObject(parentNode, "Behaviour Tree (AddChildNode)");
-
-                (parentNode as RootNode).ChildNode = null;
-
-                EditorUtility.SetDirty(parentNode);
-            }
-            else if (parentNode is DecoratorNode)
-            {
-                Undo.RecordObject(parentNode, "Behaviour Tree (AddChildNode)");
-
-                (parentNode as DecoratorNode).ChildNode = null;
-
-                EditorUtility.SetDirty(parentNode);
-            }
-            else if (parentNode is CompositeNode)
-            {
-                Undo.RecordObject(parentNode, "Behaviour Tree (AddChildNode)");
-
-                (parentNode as CompositeNode).ChildNodeList.Remove(childNode);
-
-                EditorUtility.SetDirty(parentNode);
-            }
-        }
-
-        public List<Node> GetChildren(Node parentNode)
-        {
-            if (parentNode is RootNode)
-            {
-                RootNode node = parentNode as RootNode;
-
-                return ReferenceEquals(node.ChildNode, null) ? null : new List<Node>() { node.ChildNode };
-            }
-            else if (parentNode is DecoratorNode)
-            {
-                DecoratorNode node = parentNode as DecoratorNode;
-
-                return ReferenceEquals(node.ChildNode, null) ? null : new List<Node>() { node.ChildNode };
-            }
-            else if (parentNode is CompositeNode)
-            {
-                return (parentNode as CompositeNode).ChildNodeList;
-            }
-
-            return null;
-        }
-
-        public BehaviourTree Clone()
-        {
-            BehaviourTree behaviourTree = Instantiate(this);
-
-            behaviourTree.RootNode = behaviourTree.RootNode.Clone();
-            behaviourTree.NodeList = new List<Node>();
-
-            Traverse(behaviourTree.RootNode, (node) => behaviourTree.NodeList.Add(node));
-
-            return behaviourTree;
-        }
-        
-        private void Traverse(Node node, Action<Node> nodeVisited)
-        {
-            if (ReferenceEquals(node, null)) return;
-
-            nodeVisited?.Invoke(node);
-
-            GetChildren(node)?.ForEach((childNode) => Traverse(childNode, nodeVisited));
+            rootNode.Evaluate();
         }
 
         #endregion Methods
