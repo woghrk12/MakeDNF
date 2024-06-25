295a9f7ac40e653fe86cf6ec1f4e288591121336
LAPTOP-7K9L41UJ\hjh22 <hjh222427@gmail.com>
2024-06-26 00:31:41 +0900

    Add the condition node to check whether the target is within range

---
+++ Assets/2.Script/Enemy/BehaviourTree/Component/InRangeNode.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+namespace BehaviourTree
+{
+    public class InRangeNode : ConditionNode
+    {
+        #region Variables
+
+        [SerializeField] private string dnfTransformKey = string.Empty;
+        [SerializeField] private string targetKey = string.Empty;
+        [SerializeField] private float range = 0f;
+
+        #endregion Variables
+
+        #region Methods
+
+        protected override bool CheckCondition()
+        {
+            if (!blackboard.TryGetVariable(targetKey, out DNFTransformVariable targetVariable)) return false;
+
+            if (!blackboard.TryGetVariable(dnfTransformKey, out DNFTransformVariable variable))
+            {
+                variable = gameObject.AddComponent<DNFTransformVariable>();
+
+                variable.hideFlags = HideFlags.HideInInspector;
+                variable.Value = gameObject.GetComponentInParent<DNFTransform>();
+
+                blackboard.AddVariable(dnfTransformKey, variable);
+            }
+
+            if ((variable.Value.Position - targetVariable.Value.Position).sqrMagnitude > range) return false;
+
+            return true;
+        }
+
+        #endregion Methods
+    }
+}
